869fc55b0d64655ece8827e808e487915e7a6ef0
Adam Nelip <Adam.Nelip@gmail.com>
2018-07-24 19:28:11 +0200

    Working programs

--- script-compiled.js
+++ script-compiled.js
@@ -1,4 +1,49 @@
-const hello = 'Hello';
-const world = 'World';
-const helloWorld = [...hello, ...world];
+'use strict';
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+///// Task 1
+
+var hello = 'Hello';
+var world = 'World';
+var helloWorld = hello + ' ' + world;
 console.log(helloWorld);
+
+///// Task 2
+
+var multiply = function multiply() {
+  var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return console.log(x * y);
+};
+
+///// Task 3
+
+var average = function average() {
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  return args.reduce(function (a, b) {
+    return a + b;
+  }) / args.length;
+};
+
+average(1, 3, 6, 6);
+
+///// Task 4
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+average.apply(undefined, grades);
+
+///// Task 5
+
+table = [1, 4, 'Iwona', false, 'Nowak'];
+
+var _table = table,
+    _table2 = _slicedToArray(_table, 5),
+    firstName = _table2[2],
+    lastName = _table2[4];
+
+console.log(names);
--- script.js
+++ script.js
@@ -1,5 +1,35 @@
+///// Task 1
+
 const hello = 'Hello';
 const world = 'World';
-const helloWorld = [...hello, ...world];
+const helloWorld = `${hello} ${world}`;
 console.log(helloWorld);
+
+
+///// Task 2
+
+const multiply =(x=1, y=1) => console.log(x*y);
+
+
+///// Task 3
+
+const average = (...args) => args.reduce((a, b) => a + b) / args.length;
+
+average(1, 3, 6, 6);
+
+
+///// Task 4
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+average(...grades);
+
+
+///// Task 5
+
+table = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , , firstName, , lastName] = table;
+console.log(`${firstName} ${lastName}`);
+
+
 
